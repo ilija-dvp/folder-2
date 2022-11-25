6d58fc8d44b9c0e1b78c457f0e4031bf087df3e7
ilija-dvp <118399200+ilija-dvp@users.noreply.github.com>
2022-11-25 08:41:46 +0100

    Add files via upload

---
+++ P_i_O_pravougaonika.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int main () {
+//Varijabla a
+int a;
+//Varijabla b
+int b;
+//Vrijanla P
+int P;
+//Varijabla O
+int O;
+cout<<"Unesite vrijednost za a";
+cin>>a;
+
+cout<<"Unesite vrijednost za b";
+cin>>b;
+//Obim
+O=a+b;
+//Povrsina
+P=a*b;
+
+cout<<"Povrsina pravougaonika je "<<P<<endl;
+
+cout<<"Obim pravougaonika je ";
+cout<<O<<endl;
+/*Kraj programa*/
+
+system("pause");
+return 0;
+}
